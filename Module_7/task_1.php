8e0aae16371126b990060ba53f6390a7dfd4a041
Michael <voroninmv@inbox.ru>
2021-09-27 20:04:13 +0300

    Удаление ключа в функции foreach

--- Module_7/task_1.php
+++ Module_7/task_1.php
@@ -5,7 +5,7 @@ $searchName = "test.txt";
 $searchResult = [];
 
 function searchFile ($dir, $fileName, &$resArray) {
-    foreach (scandir($dir) as $k => $v){
+    foreach (scandir($dir) as $v){
         if (is_dir($dir . '/' . $v) && $v != '.' && $v != '..') {
             searchFile($dir . '/' . $v, $fileName, $resArray);
         }
@@ -20,7 +20,7 @@ searchFile($searchRoot, $searchName, $searchResult);
 if (empty($searchResult)) {
     echo "Нет совпадений \n";
 } else {
-    foreach ($searchResult as $k => $v) {
+    foreach ($searchResult as $v) {
         echo filesize($v) > 0 ?  $v . "\n" : "Файл \"$v\" пуст \n";
     }
 }
