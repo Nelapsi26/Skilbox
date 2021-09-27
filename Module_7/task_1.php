940a6e55ce905ae17ca453b6438738e036e4b6f7
Michael <voroninmv@inbox.ru>
2021-09-27 12:16:46 +0300

    all done

--- Module_7/task_1.php
+++ Module_7/task_1.php
@@ -1,1 +1,26 @@
 <?php
+
+$searchRoot = "Test";
+$searchName = "test.txt";
+$searchResult = [];
+
+function searchFile ($dir, $fileName, &$resArray) {
+    foreach (scandir($dir) as $k => $v){
+        if (is_dir($dir . '/' . $v) && $v != '.' && $v != '..') {
+            searchFile($dir . '/' . $v, $fileName, $resArray);
+        }
+        if ($v === $fileName) {
+            $resArray[] = $dir . '/' . $v;
+        }
+    }
+}
+
+searchFile($searchRoot, $searchName, $searchResult);
+
+foreach ($searchResult as $k => $v) {
+    if (filesize($v) > 0) {
+        echo $v ;
+    }
+}
+
+
